cd6cc083af7bc9004f1dc5c488366306ea315bf9
Odysseas Papadimas <odysseas.patra@gmail.com>
2025-07-07 01:43:59 +0300

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // The base path for all assets, needed for subdirectory deployment on S3
+  base: process.env.VITE_BASE_URL || '/',
   build: {
     // Output directory for the built files
     outDir: 'dist',
